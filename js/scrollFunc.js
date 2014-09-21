70b6fb6d969dcc93f50dbc2c257b88b9e729d533
Vishrut Reddi <vishrutreddi@gmail.com>
2014-09-21 00:16:20 -0500

    Revision 1
    Sorted in folders and created the .css and .js file

---
+++ js/scrollFunc.js
@@ -0,0 +1,17 @@
+
+var pagetop, menu, yPos;
+function yScroll(){
+	pagetop = document.getElementById('pagetop');
+	menu = document.getElementById('menu');
+	yPos = window.pageYOffset;
+	if(yPos > 150){
+		pagetop.style.height = "36px";
+		pagetop.style.paddingTop = "8px";
+		menu.style.height = "0px";
+	} else {
+		pagetop.style.height = "120px";
+		pagetop.style.paddingTop = "50px";
+		menu.style.height = "50px";
+	}
+}
+window.addEventListener("scroll", yScroll);
